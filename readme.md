ae5ec0b7b3d940ab47680bd83a24490334668c03
KhromeM <68210337+KhromeM@users.noreply.github.com>
2023-03-14 15:02:59 -0400

    m

---
+++ readme.md
@@ -0,0 +1,7 @@
+Plan
+
+Due Mar 14 2023
+
+- extension activates on leetcode submissions
+
+- extension extracts code from leetcode website
--- scraper/scripts/content.js
+++ scraper/scripts/content.js
@@ -1,1 +1,52 @@
-console.log("Hello from the content");
+currUrl = window.location.href;
+console.log(currUrl);
+
+// // access the editor before submission
+// setTimeout(() => {;
+// 	// Get the file extension
+// 	const wrapper = document.querySelector(".monaco-editor");
+// 	file = wrapper.dataset.uri.split(".");
+// 	extension = file[file.length - 1];
+
+// 	// get the code
+// 	const editor = document.querySelector(".monaco-editor .view-lines");
+// 	// console.dir(editor);
+// 	code = editor.textContent;
+// 	uri = window.location.href;
+// 	parts = uri.split("/");
+// 	problemName = parts[parts.length - 2];
+// 	fileName = problemName + "." + extension;
+
+// 	console.log(fileName);
+// 	console.log(code);
+// }, 1000);
+
+const submit = () => {
+	setTimeout(() => {
+		uri = window.location.href;
+		console.log(uri);
+		parts = uri.split("/");
+		shouldSubmit = false;
+		for (const i of parts) {
+			if (i === "submissions") {
+				shouldSubmit = true;
+			}
+		}
+		if (!shouldSubmit) {
+			return;
+		}
+		problemName = parts[parts.length - 4];
+		fileName = problemName + "." + "py";
+
+		const editor = document.querySelector(".language-python");
+		code = editor.textContent;
+
+		// send to server
+	}, 3000);
+};
+
+setTimeout(() => {
+	const buttons = document.querySelectorAll(".bg-green-s");
+	const button = buttons[buttons.length - 1];
+	button.addEventListener("click", submit);
+}, 1000);
